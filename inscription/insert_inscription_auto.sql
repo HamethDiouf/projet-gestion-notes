66a5375329351de6d6709ef78b9b19353b45edbc
Hameth Diouf <dioufameth49@gmail.com>
2026-07-18 21:43:00 +0000

    Remove sensitive SQL and markdown files from repo

--- inscription/insert_inscription_auto.sql
+++
@@ -1,9 +0,0 @@
--- Insertion de données dans la table Inscription
-INSERT INTO Inscription (id_etudiant, id_niveau, date_inscription)
-SELECT id_etudiant, niveau_id,
-       CASE 
-         WHEN niveau_id <= 3 THEN DATE '2024-10-01'
-         ELSE DATE '2024-11-30'
-       END
-FROM Etudiant;
-
